0726b6e4c76d9b60e866d46793f96ee73d9185d1
hpartapur <83201298+hpartapur@users.noreply.github.com>
2023-06-12 14:36:14 +0300

    created findwords2.py that loops until a certain count of valid worsd are made

---
+++ findwords2.py
@@ -0,0 +1,100 @@
+with open('iqjaw.txt', 'r') as f:
+    words = f.read().splitlines()
+
+from tqdm import tqdm
+import json
+import random
+
+wordstr = ' '.join(words)
+
+words = wordstr.split(" ")
+
+#remove eraab characters from words in list: َ ُ ِ ً ٌ ٍ ّ ْ ٓ ٔ 
+words = [word.replace('َ', '') for word in words]
+words = [word.replace('ُ', '') for word in words]
+words = [word.replace('ِ', '') for word in words]
+words = [word.replace('ً', '') for word in words]
+words = [word.replace('ٌ', '') for word in words]
+words = [word.replace('ٍ', '') for word in words]
+words = [word.replace('ّ', '') for word in words]
+words = [word.replace('ْ', '') for word in words]
+words = [word.replace('ٓ', '') for word in words]
+words = [word.replace('ٔ', '') for word in words]
+words = [word.replace('ٰ', '') for word in words]
+
+words = [word.replace('ـ', '') for word in words]
+words = [word.replace('ثث', 'پ') for word in words]
+words = [word.replace('سس', 'ے') for word in words]
+words = [word.replace('كك', 'گ') for word in words]
+words = [word.replace('حح', 'چ') for word in words]
+words = [word.replace('ضض', 'ٹ') for word in words]
+words = [word.replace('طط', 'ں') for word in words]
+words = [word.replace('؛', 'چهے') for word in words]
+words = [word.replace('ظظ', 'ه') for word in words]
+words = [word.replace('ـ', '') for word in words]
+words = [word.replace('إِ', 'ا') for word in words]
+words = [word.replace('إ', 'ا') for word in words]
+words = [word.replace('أ', 'ا') for word in words]
+words = [word.replace('آ', 'ا') for word in words]
+words = [word.replace('‍‍ا', 'ا') for word in words]
+words = [word.replace('ؤ', 'و') for word in words]
+
+print("Finished replacements")
+
+histo={}
+
+print("Going through words and building histogram...")
+for word in tqdm(words):
+    if word in histo:
+        histo[word]+=1
+    else:
+        histo[word]=1
+
+print(len(set(words)))
+
+print(len(words))
+
+
+huroof=['ث','ں','ة','ء', 'ا', 'و', 'م', 'ز', 'پ', 'خ', 'ح', 'چ', 'ر', 'ت', 'ے', 'ط', 'ظ', 'ه', 'ض', 'د', 'ج', 'ع', 'ل', 'ك', 'گ', 'ن', 'ش', 'ص', 'غ', 'ب', 'ق', 'ف', 'ي', 'ٹ', 'ذ', 'س']
+# letters=list("جرزيفست")
+validwords=[]
+while len(validwords)<30:
+    letters=random.sample(huroof, 7)
+    words=set(words)
+
+    validwords=[]
+    invalidwords=[]
+
+    print("Filtering out words...")
+    for word in tqdm(words):
+        for w in word:
+            if w not in letters:
+                invalidwords.append(word)
+
+
+
+    invalidwords=set(invalidwords)
+
+    print("Secondary Filtering...")
+    for word in tqdm(words):
+        if word not in invalidwords and letters[3] in word:
+            validwords.append(word) 
+
+    for word in validwords:
+        print(word)
+
+
+
+x="""
+letters={}
+validWords={}
+""".format(letters, validwords)
+print(x)
+
+data={}
+data["letters"]=letters;
+data["validWords"]=validwords;
+
+data=json.dumps(data, ensure_ascii=False)
+
+print(","+data)
