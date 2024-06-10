1897ad701042120af48af099a0ccd8fce4473868
hpartapur <83201298+hpartapur@users.noreply.github.com>
2024-06-10 12:56:10 +0300

    Update scripter.js

--- scripter.js
+++ scripter.js
@@ -16,32 +16,28 @@ if (getUserMediaSupported()) {
 	console.warn("getUserMedia() is not supported by your browser");
 }
 
-// Placeholder function for next step. Paste over this in the next step.
+// Enable the live webcam view and start classification.
 function enableCam(event) {
-	// Enable the live webcam view and start classification.
-	function enableCam(event) {
-		// Only continue if the COCO-SSD has finished loading.
-		if (!model) {
-			return;
-		}
-
-		// Hide the button once clicked.
-		event.target.classList.add("removed");
-
-		// getUsermedia parameters to force video but not audio.
-		const constraints = {
-			video: true,
-		};
-
-		// Activate the webcam stream.
-		navigator.mediaDevices
-			.getUserMedia(constraints)
-			.then(function (stream) {
-				video.srcObject = stream;
-				video.addEventListener("loadeddata", predictWebcam);
-			});
+	// Only continue if the COCO-SSD has finished loading.
+	if (!model) {
+		return;
 	}
+
+	// Hide the button once clicked.
+	event.target.classList.add("removed");
+
+	// getUsermedia parameters to force video but not audio.
+	const constraints = {
+		video: true,
+	};
+
+	// Activate the webcam stream.
+	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+		video.srcObject = stream;
+		video.addEventListener("loadeddata", predictWebcam);
+	});
 }
+
 // Store the resulting model in the global scope of our app.
 var model = undefined;
 
