cddd261e282590847b5fedfded228a6d09d4e865
Hatim Partapurwala <hpartapur@gmail.com>
2025-10-20 17:42:04 +0530

    added empty service worker

---
+++ co1/service_worker.js
@@ -0,0 +1,6 @@
+// sw.js
+
+// This empty fetch handler is enough to make the PWA installable
+self.addEventListener('fetch', function(event) {
+  // Do nothing – just listen so the PWA is considered installable
+});
